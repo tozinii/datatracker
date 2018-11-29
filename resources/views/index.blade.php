2faa555acdd4d80d4b3e88a2e0b1b15c83a8bccd
Ander Gonzalez <>
2018-11-29 08:17:46 +0100

    algun que otro cambiio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title> Intechnic  </title>
+      <title> Data Tracker  </title>
       <!-- Favicon -->
       <link rel="icon" href="assets/images/favicon.png" type="image/x-icon" />
       <!-- Bootstrap CSS -->
