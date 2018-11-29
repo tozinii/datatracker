a2c97cc4a478ae14faaae58c031a5d8b58d909a7
Iker Apaolaza <iapaolazacl17dw@ikzubirimanteo.com>
2018-11-29 10:00:46 +0100

    cambios navbar terminados

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,16 +33,16 @@
                      <a class="navbar-brand logo-biss" href="index.html"> <img src="assets/images/logo.png">  </a>
                   </div>
                   <!-- end header-logo -->
-                  <div class="header-nav__list">
-                     <ul>
-                           <li class="current"><a class=""  href="/home" >Home</a></li>
-                           <li><a class=""  href="/about" >About</a></li>
-                           <li><a class=""  href="/services" >Services</a></li>
-                           <li><a class=""  href="/blog" >Blog</a></li>
-                           <li><a class="" href="/elements" >Elements</a></li>
-                           <li><a class=""  href="/contact">Contact</a></li>
-                     </ul>
-                  </nav>
+                  <div class="main-navbar">
+                        <ul>
+                              <li class="current"><a class=""  href="/home" >Home</a></li>
+                              <li><a class=""  href="/about" >About</a></li>
+                              <li><a class=""  href="/services" >Services</a></li>
+                              <li><a class=""  href="/blog" >Blog</a></li>
+                              <li><a class="" href="/elements" >Elements</a></li>
+                              <li><a class=""  href="/contact">Contact</a></li>
+                        </ul>
+                  </div>
                   
                   <nav class="header-nav">
                      <a href="#0" class="header-nav__close" title="close"><span>Close</span></a>
