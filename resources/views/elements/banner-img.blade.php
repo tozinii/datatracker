00d40f951453ee0e6c5f2dedf3ea16e040e1b2ab
cynda3 <andergonzalez33@gmail.com>
2018-12-17 08:49:58 +0100

    Pop-up register included

--- resources/views/elements/banner-img.blade.php
+++ resources/views/elements/banner-img.blade.php
@@ -15,12 +15,40 @@
                <div class="row book_now_register bg-new">
                   <div class="col-md-7 booking_text_register">
                      <h2>¡Registrate!</h2>
-                     <a id="services" href="/register" class="btn btn-primary button_12  wow fadeInUp  js-scroll-trigger" data-wow-delay=" 0.5s" style="visibility: visible; animation-delay:  0.5s; animation-name: fadeInUp;"><span class="skew_14">Registrate!</span></a>
+                     <a id="services" href="#" data-target="#registrar" data-toggle="modal" data-whatever="@mdo" class="btn btn-primary button_12  wow fadeInUp  js-scroll-trigger" data-wow-delay=" 0.5s" style="visibility: visible; animation-delay:  0.5s; animation-name: fadeInUp;"><span class="skew_14">Registrate!</span></a>
                   </div>
                </div>
             </div>
          </div>
       </div>
-
+      <!-- Pop up register -->
+      <div class="modal fade" id="registrar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Recipient:</label>
+                  <input type="text" class="form-control" id="recipient-name">
+                </div>
+                <div class="form-group">
+                  <label for="message-text" class="col-form-label">Message:</label>
+                  <textarea class="form-control" id="message-text"></textarea>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" >Close</button>
+              <button type="button" class="btn btn-primary">Send message</button>
+            </div>
+          </div>
+        </div>
+      </div><!-- se cierra pop up register -->
    </div>
 </div>
--- resources/views/layouts/layoutPrincipal.blade.php
+++ resources/views/layouts/layoutPrincipal.blade.php
@@ -17,33 +17,7 @@
                <!-- End Header_Area -->
                <!-- #banner start -->
                <section id="banner" class=" mb-90">
-                  <div class="container ">
-                     <div class="row">
-                        <!-- #banner-text start -->
-                        <div id="banner-text" class="col-md-7 text-c text-left ">
-                           <h5 class="wow fadeInUp main-h" data-wow-delay="0.2s" >Euskelec Data Tracker</h5>
-                           <p class="banner-text wow fadeInUp main-h3" data-wow-delay="0.8s">Administra tus coches y gestiona sus datos para<br> mejorar su rendimiento y capacidades. </p>
-                           <div class="top-banner wow fadeInRight">
-                              <a id="services"  href="#equipo" class="btn btn-default  wow fadeInUp  js-scroll-trigger" data-wow-delay=" 0.5s"><span class="skew_14"><i> Más información </i> </span></a>
-                           </div>
-                        </div>
-                        <!-- /#banner-text End -->
-                        <div class="our_partners_area_register ">
-                           <div class="book_now_aera_register ">
-                              <div class="container">
-                                 <div class="row book_now_register bg-new">
-                                    <div class="col-md-7 booking_text_register">
-                                          <h2>¡Registrate!
-                                          </h2>
-                                          <a id="services" href="/register" class="btn btn-primary button_12  wow fadeInUp  js-scroll-trigger" data-wow-delay=" 0.5s" style="visibility: visible; animation-delay:  0.5s; animation-name: fadeInUp;"><span class="skew_14">¡Registrate!</span></a>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-
-                     </div>
-                  </div>
+                  @include('elements.banner-img')
                </section>
                <div class="container-fluid p0 banner-shap-img">
                </div>
