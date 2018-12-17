9e9f8066f7e1a24ee4bb68f3599a22ee3757bd42
Iker Apaolaza <iapaolazacl17dw@ikzubirimanteo.com>
2018-12-17 10:30:38 +0100

    Created database and tables, started with relations.

---
+++ database/migrations/2018_12_17_090013_add_foreign_keys_to_user_group_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeysToUserGroupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_group', function (Blueprint $table) {
+          $table->foreign('user_id')->references('id')->on('users');
+          $table->foreign('group_id')->references('id')->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_group', function (Blueprint $table) {
+            //
+        });
+    }
+}
--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  //Hace submit del formulario de contacto
   $('#enviar-form-contacto').click(function(){
       $('#form-contacto').submit();
   });
