9080cc8929ea95c615ad037f7309585ae979abcd
jauregi13 <jon.j1305@gmail.com>
2018-12-20 11:34:11 +0100

    añadir link olvidar contraseña en login

--- routes/web.php
+++ routes/web.php
@@ -41,12 +41,13 @@ Route::post('register', 'Auth\RegisterController@register')->name('register');
 
 //Verify email
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
-// Password Reset Routes...
 
-//Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
-//Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-//Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
-//Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
