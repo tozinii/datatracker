08360167f661d6c25e80b09add5ee56e725e36c4
cynda3 <andergonzalez33@gmail.com>
2018-12-21 09:54:23 +0100

    groups view started

--- resources/views/users/groups.blade.php
+++ resources/views/users/groups.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.layoutLogged')
 @section('contenido')
 
-<div class="groups">
-   <h1>Grupos</h1>
-   <p>Cree un grupo o únase a uno ya existente para poder participar en los torneos de euskelec.</p>
+
+<div class="panel panel-default">
+  <!-- Default panel contents -->
+  <div class="panel-heading">Members</div>
+
+  <!-- Table -->
+  <table class="table">
+    <tr>
+    	<td>Paco</td>
+    	<td>Petardos</td>
+    </tr>
+    <tr>
+    	<td>Pepe</td>
+    	<td>Pepepe</td>
+    </tr>
+  </table>
 </div>
 
 @endsection
