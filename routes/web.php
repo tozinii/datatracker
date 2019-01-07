9104c867f3e36aa408a705fc9416a725dacdeda8
Iker Apaolaza <iapaolazacl17dw@ikzubirimanteo.com>
2019-01-07 13:43:11 +0100

    Started with server validation.

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@
 Route::get('/', function ()
 {
   return view('index');
-});
+})->name('root');
 
 //Ruta de form-contacto
 Route::post('/postContact', 'ContactController@postContact')->name('postContact');
