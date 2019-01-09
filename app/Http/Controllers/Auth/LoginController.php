ddf7d24d2756418c7e9a33174a71f65f4addf2b3
Iker Apaolaza <iapaolazacl17dw@ikzubirimanteo.com>
2019-01-09 12:14:46 +0100

    Deleted Debugbar dependencies.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
-use Debugbar;
 
 class LoginController extends Controller
 {
--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Contact;
 use Illuminate\Support\Facades\Validator;
-use Debugbar;
 
 class ContactController extends Controller
 {
