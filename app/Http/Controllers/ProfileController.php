857b4ad20355e56bc236879178829112c5591846
jauregi13 <jon.j1305@gmail.com>
2019-01-14 09:10:20 +0100

    solucionar errores en la vista perfil

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
@@ -47,7 +48,8 @@ class ProfileController extends Controller
      */
     public function show()
     {
-        return view('users.profile');
+      $user = Auth::user();
+        return view('users.profile')->with('user', $user);
     }
 
     /**
