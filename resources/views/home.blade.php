9cfb8a2140440452dfbef30bf9610f788141b013
cynda3 <andergonzalez33@gmail.com>
2019-01-18 10:49:24 +0100

    first changes in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,12 @@
 @section('contenido')
 
 <div class="container ">
-   <div class="row">
-   <!-- #banner-text start -->
-      <div id="banner-text" class="col-md-7 text-c text-left ">
-         <h5 class="wow fadeInUp main-h" data-wow-delay="0.2s" >Bienvenido {{ Auth::user()->name}}</h5>
-      </div>
-      <!-- /#banner-text End -->
-   </div>
+   <div class="alert alert-success ttl" role="alert">
+	  <h3 class="alert-heading">Bienvenido {{ Auth::user()->name}}</h3>
+	  <p>Aquí podrás crear un grupo con el cual visualizar y gestionar los datos de vuestro coche o si ya tienes un grupo, únete a el!</p>
+	  <hr>
+	  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+	</div>
 </div>
 
 @endsection
