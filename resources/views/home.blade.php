06b394954399a14b05290dba99deb2cd3c0f64a7
jauregi13 <jon.j1305@gmail.com>
2019-02-07 17:21:21 +0100

    quitar listado kits

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,30 +53,6 @@
   </table>
 
 
-<div class="card-deck">
-  @foreach($kits as $kit)
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">{{$kit->name}}</h5>
-    </div>
-    <div class="card-body">
-      <ul class="list-group list-group-flush">
-        @foreach($kit->sensors as $sensor)
-          <li class="list-group-item">{{$sensor->name}}</li>
-        @endforeach
-      </ul>
-    </div>
-    <div class="card-body">
-      <a href="" data-target="#createCar{{$kit->id}}" data-toggle="modal" class="btn btn-default  wow fadeInUp  js-scroll-trigger">Comprar</a>
-    </div>
-
-    @include('elements.pop-up-createCar')
-  </div>
-
-  @endforeach
-</div>
-
-
 
 
 
