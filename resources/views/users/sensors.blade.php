d6f4f73df4f24c513bc9b43d193dae6246cc2234
jauregi13 <jon.j1305@gmail.com>
2019-02-11 09:56:19 +0100

    solucionando ajax

--- resources/views/users/sensors.blade.php
+++ resources/views/users/sensors.blade.php
@@ -84,7 +84,7 @@
                data:  {carName : carName, sensorName: sensorName,fecha:valor,tipo:tipo} ,
                url:   '/sensorDate',
                type:  'get',
-               datatype = 'json'
+               dataType = 'json'
                success:  function (response) {
                   grafico(response);
             },
