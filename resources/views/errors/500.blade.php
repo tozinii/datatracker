ed6dd1631b27c9196bfd604dc69304d9135dc299
Iker Apaolaza <iapaolazacl17dw@ikzubirimanteo.com>
2019-02-15 12:51:09 +0100

    Added 404 page.

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.layoutErrors')
 
 @section('contenido')
-<h1>Error 405</h1>
-<p>
-  No se puede acceder a esta página.
-</p>
+  <h1>Error 500</h1>
+  <p>
+    Esta página no existe.
+  </p>
 @endsection
---
+++ resources/views/layouts/layoutErrors.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      @include('elements.head')
+   </head>
+   <body id="top">
+      @yield('contenido')
+      @include('elements.scripts')
+   </body>
+</html>
