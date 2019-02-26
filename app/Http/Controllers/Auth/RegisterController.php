261eb3997dabd22c89586fbfa9a0176e7185ba93
Iker Apaolaza <iapaolazacl17dw@ikzubirimanteo.com>
2019-02-26 11:55:52 +0100

    Fixed registration error.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'role' => 'User',
-            'api_token' =>  str_random(25)
+            'api_token' => str_random(25)
         ]);
 
 
