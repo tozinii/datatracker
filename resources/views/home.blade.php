2dd130e695ca9d8ea5326727e83e07752c1645d6
Iker Apaolaza <ikerapaolaza@gmail.com>
2019-02-28 22:20:44 +0100

    Added new links for cars at home.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,28 @@
 @section('contenido')
 
 <div class="container container-logged">
-   <div class="alert alert-success ttl" role="alert">
+   <div class="row alert alert-success ttl" role="alert">
 	  <h3 class="alert-heading">Bienvenido {{ Auth::user()->name}}</h3>
 	  <hr>
 	  <h4 class="alert-heading">Coche</h4>
     @if(Session::has('confirmation'))
       <p class="mb-0">{{Session::get('confirmation')}}</p>
-    @elseif($cars == 0)
+    @elseif(sizeOf($cars) == 0)
 	    <p class="mb-0">Actualmente usted no dispone de ningún coche.</p>
     @else
-      <p class="mb-0">Actualmente usted dispone de {{$cars}} coche(s).</p>
+      <p class="mb-0">Actualmente usted dispone de {{sizeOf($cars)}} coche(s).</p>
     @endif
 	</div>
+
+  <div class="row">
+    @foreach($cars as $car)
+    <div class="car-element">
+      <a href="{{route('cars.show',$car->id)}}"><img class="home-img align-start" src="{{asset($car->kit->image)}}"/></a>
+      <p><b>Coche:</b>{{ $car->code }}</p>
+      <p><b>Kit:</b>{{ $car->kit->name }}</p>           
+    </div>
+    @endforeach
+  </div>
 
 </div>
 @endsection
