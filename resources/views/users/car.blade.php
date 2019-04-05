c7cf0b0f78b849942f229d819286c00dc0d0ccb9
jonva12 <jonva12@hotmail.com>
2019-04-05 03:00:57 +0200

    bateria bien

--- resources/views/users/car.blade.php
+++ resources/views/users/car.blade.php
@@ -83,22 +83,25 @@ $(document).ready(function () {
 		  			if($("#sensor"+mostrar) && dato[i]){
 		  				$("#sensor"+mostrar).val(dato[i].data);
 		  				document.getElementById(mostrar).style.display = "block";
-		  			}
-		  		}
-		  		//Esto funciona
-		  		document.getElementById("1").style.display = "block";
-		  		//$("velocimetro").style.display = "block";
-		  		if(dato[1].data < 25){
-		  			document.getElementById("bateria1").style.display = "block";
-		  		}else if(dato[1].data >= 25 && dato[2].data < 50){
-		  			document.getElementById("bateria2").style.display = "block";
-		  		}else if(dato[1].data >= 50 && dato[2].data < 75){
-		  			document.getElementById("bateria3").style.display = "block";
-		  		}else{
-		  			document.getElementById("bateria1").style.display = "block";
-		  		}
+		  				if(dato[1].data < 20){
+		  					document.getElementById("bateria1").style.display = "block";
+		  					console.log(dato[1].data);
+				  		}else if(dato[1].data < 51){
+				  			console.log(dato[1].data);
+				  			document.getElementById("bateria2").style.display = "block";
+				  		}else if(dato[1].data < 90){
+				  			console.log(dato[1].data);
+				  			document.getElementById("bateria3").style.display = "block";
+				  		}else{
+				  			document.getElementById("bateria4").style.display = "block";
+				  			console.log(dato[1].data);
+				  		}
+				  	}
+				}
 
-		  		//Esto deberia cambiarlo
+		  		//$("velocimetro").style.display = "block";
+
+
 		  		var rotar;
 		  		if(dato[0].data != 0){
 		  			rotar = rotar = (405*dato[0].data)/50;
@@ -120,73 +123,6 @@ $(document).ready(function () {
 		  				console.log(rotar);
 		  			}
 		  		}
-		 		
-		  		
-			  	/*var newRow2 = $("2").val = dato[1].data;
-			  	var newRow3 = $("3").val = dato[2].data;
-			  	var newRow4 = $("4").val = dato[3].data;
-			  	var newRow5 = $("5").val = dato[4].data;
-			  	var newRow6 = $("6").val = dato[5].data;
-			  	var newRow7 = $("7").val = dato[6].data;
-			  	var newRow8 = $("8").val = dato[7].data;
-			  	var newRow9 = $("9").val = dato[8].data;
-			  	var newRow10 = $("10").val = dato[9].data;
-			  	var newRow11 = $("11").val = dato[10].data;
-			  	var newRow12 = $("12").val = dato[11].data;*/
-			
-		  		/*switch(kit) {
-				  case "1":
-				  	valores = [1,2,3,4,5,10];
-				    for(var i = 0; i < dato.length; i++){
-			  			if (dato[i] != null) {
-			  				var newRow = "<input type='text' id='valor' value="+dato[i].data+" disabled>";
-			  				$(newRow).appendTo("#"+dato[i].sensor_id);
-			  			}else{
-			  				var newRow = "<input type='text' id='valor' value='Sin Valor' disabled>";
-			  				$(newRow).appendTo("#"+valores[i]);
-		  				}
-		  			}
-				    break;
-				  case "2":
-				    valores = [1,2,3,4,5,7,10,11,12];
-				    for(var i = 0; i < dato.length; i++){
-			  			if (dato[i] != null) {
-			  				var newRow = "<input type='text' id='valor' value="+dato[i].data+" disabled>";
-			  				$(newRow).appendTo("#"+dato[i].sensor_id);
-			  			}else{
-			  				var newRow = "<input type='text' id='valor' value='Sin Valor' disabled>";
-			  				$(newRow).appendTo("#"+valores[i]);
-		  				}
-		  			}
-				    break;
-				  case "3":
-				    valores = [1,2,3,4,5,6,7,10,11,12];
-				    for(var i = 0; i < dato.length; i++){
-			  			if (dato[i] != null) {
-			  				var newRow = "<input type='text' id='valor' value="+dato[i].data+" disabled>";
-			  				$(newRow).appendTo("#"+dato[i].sensor_id);
-			  			}else{
-			  				var newRow = "<input type='text' id='valor' value='Sin Valor' disabled>";
-			  				$(newRow).appendTo("#"+valores[i]);
-		  				}
-		  			}
-				   	break;
-				  case "4":
-				    for(var i = 0; i < dato.length; i++){
-			  			if (dato[i] != null) {
-			  				console.log(i);
-			  				var newRow = "<input type='text' id='valor' value="+dato[i].data+" disabled>";
-			  				$(newRow).appendTo("#"+dato[i].sensor_id);
-			  			}else{
-			  				var mostrar = i+1;
-			  				var newRow = "<input type='text' id='valor' value='Sin Valor' disabled>";
-			  				$(newRow).appendTo("#"+mostrar);
-		  				}
-		  			}
-				    break;
-				  default:
-				    alert("sa matao paco");
-				}*/
 	       }
 		});
 });
